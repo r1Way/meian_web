234645422fd509f21af29ca27b373ae105c84758
r1Way <2142341323@qq.com>
2025-01-27 22:53:26 +0800

    1.开始开发User ····· 遇到了问题，没有外键

--- djangotutorial/home/admin.py
+++ djangotutorial/home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import UserProfile, Comment
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(Comment)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    # can_delete = False
+    # verbose_name_plural = 'userprofile'
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [UserProfileInline]#父类就有的为空 将附加信息内联
+
+admin.site.register(User, UserProfileAdmin)
--- djangotutorial/home/models.py
+++ djangotutorial/home/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    nike_name = models.CharField(max_length=20)
+    # username = models.CharField(max_length=20)
+    # password = models.CharField(max_length=20)
+
+class Comment(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     date = models.DateTimeField(auto_now_add=True)
+#     author = models.CharField(max_length=20)
+#     comment = models.ManyToManyField(Comment, blank=True)
